c3210bd2e5908af750151354a7965b86db1bd137
EdwardGoomba <ask@edwardvdesign.com>
2017-12-20 10:24:28 -0500

    Added friendlier location hover

---
+++ app.js
@@ -0,0 +1,8 @@
+// Changes location to more friendly on hover
+$('.loc').hover(
+  function(){
+    $(this).html("<strong>Location:</strong> Your house?!");
+  },
+  function() {
+    $(this).html("<strong>Location:</strong> Treehouse Adoption Center");
+});
