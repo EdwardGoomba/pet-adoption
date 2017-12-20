019aa93b3dfebfadbd50074b669ac11918366bbd
EdwardGoomba <ask@edwardvdesign.com>
2017-12-20 10:26:43 -0500

    Adds info card below form submit

--- app.js
+++ app.js
@@ -6,3 +6,26 @@ $('.loc').hover(
   function() {
     $(this).html("<strong>Location:</strong> Treehouse Adoption Center");
 });
+
+// Adds a pet to the page with user input
+
+$('#add-pet').on('click', function() {
+  // Grab info from form
+  let $name = $('#pet-name');
+  let $species = $('#pet-species');
+  let $notes = $('#pet-notes');
+
+  // Assemble the HTML of new element with above variables
+  let $newPet = $(
+  '<section class="six columns"><div class="card"><p><strong>Name:</strong> ' + $name.val() +
+  '</p><strong>Species:</strong> ' + $species.val() +
+  '</p><p><strong>Notes:</strong> ' + $notes.val() +
+  '</p><span class="close">&times;</span></div></section>'
+
+  );
+
+  // Attach the new element to page
+
+  $('#posted-pets').append($newPet);
+
+});
